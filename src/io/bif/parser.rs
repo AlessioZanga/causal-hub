aedc4a89df00b72f9c1ee9ffb6b9aa8add44c5f1
AlessioZanga <alessio.zanga@outlook.it>
2023-03-08 18:02:30 +0100

    Update `BIF` parser

--- src/io/bif/parser.rs
+++ src/io/bif/parser.rs
@@ -1,5 +1,7 @@
 use std::{io::Error as IOError, path::PathBuf};
 
+use itertools::Itertools;
+use ndarray::Array2;
 use pest::{
     error::Error as ParserError,
     iterators::{Pair, Pairs},
@@ -7,15 +9,110 @@ use pest::{
 };
 use pest_derive::Parser;
 
-use crate::io::File;
+use crate::{
+    io::File,
+    models::DiscreteCPD,
+    prelude::{FxIndexMap, FxIndexSet},
+};
 
 #[derive(Clone, Debug, Default, Parser)]
 #[grammar = "io/bif/grammar.pest"]
-pub struct BayesianInterchangeFormat {}
+pub struct BayesianInterchangeFormat {
+    /// CPDs set. TODO: Generalize to the continuous case.
+    pub cpds: Vec<DiscreteCPD>,
+}
 
-impl<'a> From<Pair<'a, Rule>> for BayesianInterchangeFormat {
-    fn from(pair: Pair<'a, Rule>) -> Self {
-        todo!() // FIXME:
+impl<'a> From<Pairs<'a, Rule>> for BayesianInterchangeFormat {
+    fn from(pairs: Pairs<'a, Rule>) -> Self {
+        // Initialize CPDs vector.
+        let mut tables: Vec<DiscreteCPD> = Default::default();
+        // Initialize scopes map.
+        let mut scopes: FxIndexMap<String, FxIndexSet<String>> = Default::default();
+
+        // Match inner rules.
+        let mut inner = pairs;
+
+        // Assert rule match. TODO: Parse network properties.
+        let network = inner.next().unwrap();
+        assert!(matches!(network.as_rule(), Rule::network_declaration));
+
+        // Assert rule match.
+        for variable_probability in inner {
+            match variable_probability.as_rule() {
+                Rule::variable_declaration => {
+                    // Match inner rules.
+                    let mut i = variable_probability.into_inner();
+
+                    // Assert rule match.
+                    let name = i.next().unwrap();
+                    assert!(matches!(name.as_rule(), Rule::variable_name));
+                    // Get variable name.
+                    let name = name.as_str().into();
+
+                    // Assert rule match.
+                    let content = i.next().unwrap();
+                    assert!(matches!(content.as_rule(), Rule::variable_content));
+                    // Match inner rules. TODO: Generalize to the continuous case.
+                    let mut i = content.into_inner();
+
+                    // Assert rule match.
+                    let discrete = i.next().unwrap();
+                    assert!(matches!(discrete.as_rule(), Rule::variable_discrete));
+                    // Match inner rules.
+                    let mut i = discrete.into_inner();
+
+                    // Assert rule match.
+                    let states = i.next().unwrap();
+                    assert!(matches!(states.as_rule(), Rule::variable_states_list));
+                    // Collect states.
+                    let states = states.into_inner().map(|s| s.as_str().into()).collect();
+
+                    // Insert variable with states into scope.
+                    scopes.insert(name, states);
+                }
+                Rule::probability_declaration => {
+                    // Match inner rules.
+                    let mut i = variable_probability.into_inner();
+
+                    // Assert rule match.
+                    let variables = i.next().unwrap();
+                    assert!(matches!(
+                        variables.as_rule(),
+                        Rule::probability_variables_list
+                    ));
+                    // Get variable name.
+                    let variables: Vec<_> = variables
+                        .into_inner()
+                        .map(|x| x.as_str().to_owned())
+                        .collect();
+
+                    // Assert rule match.
+                    let content = i.next().unwrap();
+                    assert!(matches!(content.as_rule(), Rule::probability_content));
+
+                    // Initialize CPDs values. TODO: Generalize to the continuous case.
+                    let mut values: Vec<Vec<f64>> = Default::default();
+                    // Match into inner rules.
+                    for entry in content.into_inner() {
+                        match entry.as_rule() {
+                            Rule::probability_default_entry | Rule::probability_table => values
+                                .push(
+                                    entry
+                                        .into_inner()
+                                        .map(|x| x.as_str().parse().unwrap())
+                                        .collect(),
+                                ),
+                            Rule::probability_entry => {}
+                            _ => unreachable!(),
+                        }
+                    }
+                }
+                Rule::EOI => {}
+                _ => unreachable!(),
+            }
+        }
+
+        Default::default() // FIXME:
     }
 }
 
@@ -32,7 +129,7 @@ impl TryFrom<String> for BayesianInterchangeFormat {
         // Parse the given string.
         let out = Self::parse(Rule::compilation_unit, string.trim())?;
         // Match inner rules.
-        let out: Self = Self {}; // FIXME: out.map(|x| x.into()).next().unwrap();
+        let out: Self = out.into();
 
         Ok(out)
     }
