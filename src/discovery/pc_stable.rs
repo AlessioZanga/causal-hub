b7934643c9595a7bf681e37312726d413f63e627
Niccolo-Rocchi <nik9991u@gmail.com>
2023-03-21 11:00:30 +0100

    Update: improved complexity

--- src/discovery/pc_stable.rs
+++ src/discovery/pc_stable.rs
@@ -7,19 +7,22 @@ pub fn skeleton(test: &ChiSquared, mut g: Graph) -> Graph {
     let mut c = 0;
     while flag {
         flag = false;
-        let g_prime = g.clone();
-        for (x, y) in g_prime.edges() {
+        let mut e_prime = Vec::with_capacity(g.size());
+        for (x, y) in g.edges() {
             let adj = iter_set::union(
-                g_prime.adjacents(x).filter(|v| *v != y).combinations(c),
-                g_prime.adjacents(y).filter(|v| *v != x).combinations(c),
+                g.adjacents(x).filter(|&v| v != y).combinations(c),
+                g.adjacents(y).filter(|&v| v != x).combinations(c),
             );
             for z in adj {
                 flag = true;
                 if !test.call(x, y, &z) {
-                    g.del_edge(x, y);
+                    e_prime.push((x, y));
                     break;
                 }
             }
+        }
+        for (x, y) in e_prime {
+            g.del_edge(x, y);
         }
         c += 1;
     }
