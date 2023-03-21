3b816bbbf14a0ac5a1946d764efa3fa5a04857ea
Niccolo-Rocchi <nik9991u@gmail.com>
2023-03-21 11:41:01 +0100

    Update: generalization of types

--- src/discovery/pc_stable.rs
+++ src/discovery/pc_stable.rs
@@ -2,16 +2,20 @@ use crate::prelude::*;
 use itertools::Itertools;
 
 /// Skeleton function, as part of PC-Stable algorithm. Inputs: test over data and a complete graph over data nodes.
-pub fn skeleton(test: &ChiSquared, mut g: Graph) -> Graph {
+pub fn skeleton<T, G>(test: &T, mut g: G) -> G
+where
+    T: ConditionalIndependenceTest,
+    G: BaseGraph<Direction = directions::Undirected>,
+{
     let mut flag = true;
     let mut c = 0;
     while flag {
         flag = false;
         let mut e_prime = Vec::with_capacity(g.size());
-        for (x, y) in g.edges() {
+        for (x, y) in E!(g) {
             let adj = iter_set::union(
-                g.adjacents(x).filter(|&v| v != y).combinations(c),
-                g.adjacents(y).filter(|&v| v != x).combinations(c),
+                Adj!(g, x).filter(|&v| v != y).combinations(c),
+                Adj!(g, y).filter(|&v| v != x).combinations(c),
             );
             for z in adj {
                 flag = true;
