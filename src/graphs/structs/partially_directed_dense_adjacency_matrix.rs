f5838d4bcf36acfde81ccbb3143fe3109a6e9cbb
Niccolo-Rocchi <nik9991u@gmail.com>
2023-03-23 18:01:09 +0100

    Add: `BaseGraph` implementation

--- src/graphs/structs/partially_directed_dense_adjacency_matrix.rs
+++ src/graphs/structs/partially_directed_dense_adjacency_matrix.rs
@@ -303,7 +303,10 @@ impl Display for PartiallyDenseAdjacencyMatrixGraph {
 impl Hash for PartiallyDenseAdjacencyMatrixGraph {
     #[inline]
     fn hash<H: Hasher>(&self, state: &mut H) {
-        todo!() //TODO: 
+        self.labels.hash(state);
+        self.directed_adjacency_matrix.hash(state);
+        self.undirected_adjacency_matrix.hash(state);
+        self.skeleton_adjacency_matrix.hash(state);
     }
 }
 
@@ -340,74 +343,512 @@ impl BaseGraph for PartiallyDenseAdjacencyMatrixGraph {
         I: IntoIterator<Item = V>,
         J: IntoIterator<Item = (V, V)>,
     {
-        todo!()
+        // Remove duplicated vertices labels.
+        let mut labels: BTreeSet<_> = vertices.into_iter().map(|x| x.into()).collect();
+        // Map edges iterator into edge list.
+        let edges: EdgeList<_> = edges
+            .into_iter()
+            .map(|(x, y)| (x.into(), y.into()))
+            .collect();
+        // Add missing vertices from the edges.
+        labels.extend(edges.iter().cloned().flat_map(|(x, y)| [x, y]));
+
+        // Compute new graph order.
+        let order = labels.len();
+        // Map vertices labels to vertices indices.
+        let labels_indices: BiHashMap<_, _> = labels
+            .iter()
+            .cloned()
+            .enumerate()
+            .map(|(i, x)| (x, i))
+            .collect();
+
+        // Initialize skeleton adjacency matrix given graph order.
+        let mut skeleton_adjacency_matrix = DenseAdjacencyMatrix::from_elem((order, order), false);
+
+        // Initialize the size.
+        let mut size = 0;
+        // Fill skeleton adjacency matrix given edge set.
+        for (x, y) in edges {
+            // Get associated vertices indices.
+            let (i, j) = (
+                *labels_indices.get_by_left(&x).unwrap(),
+                *labels_indices.get_by_left(&y).unwrap(),
+            );
+            // Set edge given indices.
+            if !skeleton_adjacency_matrix[[i, j]] {
+                // Add edge.
+                skeleton_adjacency_matrix[[i, j]] = true;
+                skeleton_adjacency_matrix[[j, i]] = true;
+                // Increment size.
+                size += 1;
+            }
+        }
+
+        // Instantiate undirected adjacency matrix as a clone of skeleton adjacency matrix.
+        let undirected_adjacency_matrix = skeleton_adjacency_matrix.clone();
+
+        // Instantiate empty directed adjacency matrix given graph order.
+        let directed_adjacency_matrix = DenseAdjacencyMatrix::from_elem((order, order), false);
+
+        Self {
+            labels,
+            labels_indices,
+            undirected_adjacency_matrix,
+            directed_adjacency_matrix,
+            skeleton_adjacency_matrix,
+            size,
+        }
     }
 
+    #[inline]
     fn clear(&mut self) {
-        todo!()
+        // Clear the vertices.
+        self.labels.clear();
+        // Clear the vertices map.
+        self.labels_indices.clear();
+        // Clear all adjacency matrices.
+        self.undirected_adjacency_matrix = Default::default();
+        self.directed_adjacency_matrix = Default::default();
+        self.skeleton_adjacency_matrix = Default::default();
+        // Clear the size.
+        self.size = 0;
     }
 
+    #[inline]
     fn label(&self, x: usize) -> &str {
-        todo!()
+        self.labels_indices
+            .get_by_right(&x)
+            .unwrap_or_else(|| panic!("No vertex with label `{x}`"))
     }
 
+    #[inline]
     fn labels(&self) -> Self::LabelsIter<'_> {
-        todo!()
+        Self::LabelsIter::new(self)
     }
 
+    #[inline]
     fn vertex(&self, x: &str) -> usize {
-        todo!()
+        *self
+            .labels_indices
+            .get_by_left(x)
+            .unwrap_or_else(|| panic!("No vertex with identifier `{x}`"))
     }
 
+    #[inline]
     fn vertices(&self) -> Self::VerticesIter<'_> {
-        todo!()
+        // Assert vertex set and vertices map are consistent.
+        debug_assert_eq!(self.labels.len(), self.labels_indices.len());
+
+        0..self.labels.len()
     }
 
+    #[inline]
     fn order(&self) -> usize {
-        V!(self).len()
+        // Check iterator consistency.
+        debug_assert_eq!(V!(self).len(), self.labels.len());
+        // Assert vertex set and vertices map are consistent.
+        debug_assert_eq!(self.labels.len(), self.labels_indices.len());
+
+        // Assert vertex set is consistent with adjacency matrices shapes.
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.undirected_adjacency_matrix.nrows()
+        );
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.directed_adjacency_matrix.nrows()
+        );
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.skeleton_adjacency_matrix.nrows()
+        );
+
+        // Assert adjacency matrices are square.
+        debug_assert!(self.undirected_adjacency_matrix.is_square());
+        debug_assert!(self.directed_adjacency_matrix.is_square());
+        debug_assert!(self.skeleton_adjacency_matrix.is_square());
+
+        self.labels.len()
     }
 
+    #[inline]
     fn has_vertex(&self, x: usize) -> bool {
-        V!(self).any(|y| y == x)
+        // Check vertex existence.
+        let f = self.labels_indices.contains_right(&x);
+
+        // Check iterator consistency.
+        debug_assert_eq!(V!(self).any(|y| y == x), f);
+        // Assert vertex set and vertices map are consistent.
+        debug_assert_eq!(x < self.order(), f);
+
+        f
     }
 
     fn add_vertex<V>(&mut self, x: V) -> usize
     where
         V: Into<String>,
     {
-        todo!()
+        // Cast vertex label.
+        let x = x.into();
+
+        // If vertex was already present ...
+        if !self.labels.insert(x.clone()) {
+            // ... return early.
+            return self.vertex(&x);
+        }
+
+        // Get vertex identifier.
+        let i = self.labels.iter().position(|y| y == &x).unwrap();
+
+        // Update the vertices map after the added vertex.
+        for (j, y) in self.labels.iter().skip(i).enumerate() {
+            // Add the given vertex and increment subsequent ones by overwriting the entries.
+            self.labels_indices.insert(y.clone(), i + j);
+        }
+
+        // Compute the new size of adjacency matrix.
+        let n = self.skeleton_adjacency_matrix.nrows();
+        // Allocate new adjacency matrices.
+        let mut directed_adjacency_matrix = DenseAdjacencyMatrix::from_elem((n + 1, n + 1), false);
+        let mut undirected_adjacency_matrix =
+            DenseAdjacencyMatrix::from_elem((n + 1, n + 1), false);
+        let mut skeleton_adjacency_matrix = DenseAdjacencyMatrix::from_elem((n + 1, n + 1), false);
+        // Compute blocks.
+        let (p, q) = ([0..i, (i + 1)..(n + 1)], [0..i, i..n]);
+        let (p, q) = (iproduct!(p.clone(), p), iproduct!(q.clone(), q));
+        // Copy old adjacency matrix using blocks operations.
+        for ((p_start, p_end), (q_start, q_end)) in p.zip(q) {
+            directed_adjacency_matrix
+                .slice_mut(s![p_start.clone(), p_end.clone()])
+                .assign(
+                    &self
+                        .directed_adjacency_matrix
+                        .slice(s![q_start.clone(), q_end.clone()]),
+                );
+
+            undirected_adjacency_matrix
+                .slice_mut(s![p_start.clone(), p_end.clone()])
+                .assign(
+                    &self
+                        .undirected_adjacency_matrix
+                        .slice(s![q_start.clone(), q_end.clone()]),
+                );
+
+            skeleton_adjacency_matrix
+                .slice_mut(s![p_start, p_end])
+                .assign(&self.skeleton_adjacency_matrix.slice(s![q_start, q_end]));
+        }
+        // Replace old with new adjacency matrices.
+        self.directed_adjacency_matrix = directed_adjacency_matrix;
+        self.undirected_adjacency_matrix = undirected_adjacency_matrix;
+        self.skeleton_adjacency_matrix = skeleton_adjacency_matrix;
+
+        // Assert vertex has been added.
+        debug_assert!(self.labels.contains(&x));
+        debug_assert!(self.labels_indices.contains_left(&x));
+        // Assert vertex set is still consistent with vertices map.
+        debug_assert!(self
+            .labels
+            .iter()
+            .eq(self.labels_indices.left_values().sorted()));
+        // Assert vertices labels are still associated to an ordered and
+        // contiguous sequence of integers starting from zero, i.e in [0, n).
+        debug_assert!(self
+            .labels_indices
+            .right_values()
+            .cloned()
+            .sorted()
+            .eq(0..self.labels_indices.len()));
+        // Assert vertex set is still consistent with adjacency matrices shapes.
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.directed_adjacency_matrix.nrows()
+        );
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.undirected_adjacency_matrix.nrows()
+        );
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.skeleton_adjacency_matrix.nrows()
+        );
+        // Assert adjacency matrices are still square.
+        debug_assert!(self.directed_adjacency_matrix.is_square());
+        debug_assert!(self.undirected_adjacency_matrix.is_square());
+        debug_assert!(self.skeleton_adjacency_matrix.is_square());
+        // Assert adjacency matrices are still symmetric.
+        debug_assert_eq!(
+            self.undirected_adjacency_matrix,
+            self.undirected_adjacency_matrix.t()
+        );
+        debug_assert_eq!(
+            self.skeleton_adjacency_matrix,
+            self.skeleton_adjacency_matrix.t()
+        );
+
+        // Return new vertex.
+        i
     }
 
     fn del_vertex(&mut self, x: usize) -> bool {
-        todo!()
+        // Get vertex label and identifier.
+        let x_i = self.labels_indices.remove_by_right(&x);
+
+        // If vertex was not present ...
+        if x_i.is_none() {
+            // ... then return early.
+            return false;
+        }
+
+        // Get vertex label and identifier.
+        let (x, i) = x_i.unwrap();
+
+        // Remove vertex label.
+        self.labels.remove(&x);
+
+        // Update the vertices map after the removed vertex.
+        for (j, y) in self.labels.iter().skip(i).enumerate() {
+            // Decrement subsequent ones by overwriting the entries.
+            self.labels_indices.insert(y.clone(), i + j);
+        }
+
+        // Compute the new size of adjacency matrix.
+        let n = self.skeleton_adjacency_matrix.nrows();
+        // Allocate new adjacency matrices.
+        let mut directed_adjacency_matrix = DenseAdjacencyMatrix::from_elem((n - 1, n - 1), false);
+        let mut undirected_adjacency_matrix =
+            DenseAdjacencyMatrix::from_elem((n - 1, n - 1), false);
+        let mut skeleton_adjacency_matrix = DenseAdjacencyMatrix::from_elem((n - 1, n - 1), false);
+        // Compute blocks.
+        let (p, q) = ([0..i, i..(n - 1)], [0..i, (i + 1)..n]);
+        let (p, q) = (iproduct!(p.clone(), p), iproduct!(q.clone(), q));
+        // Copy old adjacency matrix using blocks operations.
+        for ((p_start, p_end), (q_start, q_end)) in p.zip(q) {
+            directed_adjacency_matrix
+                .slice_mut(s![p_start.clone(), p_end.clone()])
+                .assign(
+                    &self
+                        .directed_adjacency_matrix
+                        .slice(s![q_start.clone(), q_end.clone()]),
+                );
+
+            undirected_adjacency_matrix
+                .slice_mut(s![p_start.clone(), p_end.clone()])
+                .assign(
+                    &self
+                        .undirected_adjacency_matrix
+                        .slice(s![q_start.clone(), q_end.clone()]),
+                );
+
+            skeleton_adjacency_matrix
+                .slice_mut(s![p_start, p_end])
+                .assign(&self.skeleton_adjacency_matrix.slice(s![q_start, q_end]));
+        }
+        // Replace old with new adjacency matrices.
+        self.directed_adjacency_matrix = directed_adjacency_matrix;
+        self.undirected_adjacency_matrix = undirected_adjacency_matrix;
+        self.skeleton_adjacency_matrix = skeleton_adjacency_matrix;
+
+        // Assert vertex has been removed.
+        debug_assert!(!self.labels.contains(&x));
+        debug_assert!(!self.labels_indices.contains_left(&x));
+        // Assert vertex set is still consistent with vertices map.
+        debug_assert!(self
+            .labels
+            .iter()
+            .eq(self.labels_indices.left_values().sorted()));
+        // Assert vertices labels are still associated to an ordered and
+        // contiguous sequence of integers starting from zero, i.e in [0, n).
+        debug_assert!(self
+            .labels_indices
+            .right_values()
+            .cloned()
+            .sorted()
+            .eq(0..self.labels_indices.len()));
+        // Assert vertex set is still consistent with adjacency matrices shapes.
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.directed_adjacency_matrix.nrows()
+        );
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.undirected_adjacency_matrix.nrows()
+        );
+        debug_assert_eq!(
+            self.labels_indices.len(),
+            self.skeleton_adjacency_matrix.nrows()
+        );
+        // Assert adjacency matrices are still square.
+        debug_assert!(self.directed_adjacency_matrix.is_square());
+        debug_assert!(self.undirected_adjacency_matrix.is_square());
+        debug_assert!(self.skeleton_adjacency_matrix.is_square());
+        // Assert adjacency matrices are still symmetric.
+        debug_assert_eq!(
+            self.undirected_adjacency_matrix,
+            self.undirected_adjacency_matrix.t()
+        );
+        debug_assert_eq!(
+            self.skeleton_adjacency_matrix,
+            self.skeleton_adjacency_matrix.t()
+        );
+
+        true
     }
 
+    #[inline]
     fn edges(&self) -> Self::EdgesIter<'_> {
-        todo!()
+        Self::EdgesIter::new(self)
     }
 
+    #[inline]
     fn size(&self) -> usize {
-        E!(self).len()
+        // Check iterator consistency.
+        debug_assert_eq!(E!(self).len(), self.size);
+
+        self.size
     }
 
+    #[inline]
     fn has_edge(&self, x: usize, y: usize) -> bool {
-        E!(self).any(|z| z == (x, y))
+        self.skeleton_adjacency_matrix[[x, y]]
     }
 
+    #[inline]
     fn add_edge(&mut self, x: usize, y: usize) -> bool {
-        todo!()
+        // If edge already exists ...
+        if self.skeleton_adjacency_matrix[[x, y]] {
+            // ... return early.
+            return false;
+        }
+
+        // Add edge.
+        self.undirected_adjacency_matrix[[x, y]] = true;
+        self.undirected_adjacency_matrix[[y, x]] = true;
+
+        self.skeleton_adjacency_matrix[[x, y]] = true;
+        self.skeleton_adjacency_matrix[[y, x]] = true;
+
+        // Increment size.
+        self.size += 1;
+
+        // Assert adjacency matrices are still consistent.
+        debug_assert_eq!(
+            self.undirected_adjacency_matrix[[x, y]],
+            self.undirected_adjacency_matrix[[y, x]]
+        );
+        debug_assert_eq!(
+            self.skeleton_adjacency_matrix[[x, y]],
+            self.skeleton_adjacency_matrix[[y, x]]
+        );
+        // Assert size counter and adjacency matrices are still consistent.
+        debug_assert_eq!(
+            self.size,
+            self.undirected_adjacency_matrix
+                .indexed_iter()
+                .filter_map(|((i, j), &f)| match i <= j {
+                    true => Some(f as usize),
+                    false => None,
+                })
+                .sum()
+        );
+
+        debug_assert_eq!(
+            self.size,
+            self.skeleton_adjacency_matrix
+                .indexed_iter()
+                .filter_map(|((i, j), &f)| match i <= j {
+                    true => Some(f as usize),
+                    false => None,
+                })
+                .sum()
+        );
+
+        true
     }
 
+    #[inline]
     fn del_edge(&mut self, x: usize, y: usize) -> bool {
-        todo!()
+        // If edge does not exists ...
+        if !self.skeleton_adjacency_matrix[[x, y]] {
+            // ... return early.
+            return false;
+        }
+
+        // Remove edge.
+        self.undirected_adjacency_matrix[[x, y]] = false;
+        self.undirected_adjacency_matrix[[y, x]] = false;
+
+        self.skeleton_adjacency_matrix[[x, y]] = false;
+        self.skeleton_adjacency_matrix[[y, x]] = false;
+
+        // Decrement size.
+        self.size -= 1;
+
+        // Assert adjacency matrices are still consistent.
+        debug_assert_eq!(
+            self.undirected_adjacency_matrix[[x, y]],
+            self.undirected_adjacency_matrix[[y, x]]
+        );
+        debug_assert_eq!(
+            self.skeleton_adjacency_matrix[[x, y]],
+            self.skeleton_adjacency_matrix[[y, x]]
+        );
+        // Assert size counter and adjacency matrices are still consistent.
+        debug_assert_eq!(
+            self.size,
+            self.undirected_adjacency_matrix
+                .indexed_iter()
+                .filter_map(|((i, j), &f)| match i <= j {
+                    true => Some(f as usize),
+                    false => None,
+                })
+                .sum()
+        );
+
+        debug_assert_eq!(
+            self.size,
+            self.skeleton_adjacency_matrix
+                .indexed_iter()
+                .filter_map(|((i, j), &f)| match i <= j {
+                    true => Some(f as usize),
+                    false => None,
+                })
+                .sum()
+        );
+
+        true
     }
 
+    #[inline]
     fn adjacents(&self, x: usize) -> Self::AdjacentsIter<'_> {
-        todo!()
+        Self::AdjacentsIter::new(self, x)
     }
 
+    #[inline]
     fn is_adjacent(&self, x: usize, y: usize) -> bool {
-        Adj!(self, x).any(|z| z == y)
+        // Check using has_edge.
+        let f = self.has_edge(x, y);
+
+        // Check iterator consistency.
+        debug_assert_eq!(Adj!(self, x).any(|z| z == y), f);
+
+        f
+    }
+}
+
+/* Implement DefaultGraph trait. */
+impl Default for PartiallyDenseAdjacencyMatrixGraph {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            labels: Default::default(),
+            labels_indices: Default::default(),
+            undirected_adjacency_matrix: DenseAdjacencyMatrix::from_elem((0, 0), false),
+            directed_adjacency_matrix: DenseAdjacencyMatrix::from_elem((0, 0), false),
+            skeleton_adjacency_matrix: DenseAdjacencyMatrix::from_elem((0, 0), false),
+            size: 0,
+        }
     }
 }
 
