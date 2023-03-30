ea1bcd5e0b457ba17bfbb958444846ec9d22f9cc
Niccolo-Rocchi <nik9991u@gmail.com>
2023-03-30 16:25:56 +0200

    Add: `Into<DOT>` for partially directed graph

--- src/io/dot/parser.rs
+++ src/io/dot/parser.rs
@@ -14,11 +14,11 @@ use super::{
 };
 use crate::{
     graphs::{
-        structs::{DirectedDenseAdjacencyMatrixGraph, UndirectedDenseAdjacencyMatrixGraph},
-        BaseGraph,
+        structs::{DirectedDenseAdjacencyMatrixGraph, UndirectedDenseAdjacencyMatrixGraph, PartiallyDenseAdjacencyMatrixGraph},
+        BaseGraph, PartiallyGraph, 
     },
     io::File,
-    E, V,
+    uE, dE, E, V,
 };
 
 impl<'a> Extend<Pair<'a, Rule>> for VertexAttributes {
@@ -649,6 +649,42 @@ impl From<DirectedDenseAdjacencyMatrixGraph> for DOT {
             .map(|(x, y)| Edge::new((x, y), "->".into()))
             .map(|x| (x.id.clone(), x))
             .collect();
+
+        Self {
+            graph_type,
+            vertices,
+            edges,
+            ..Default::default()
+        }
+    }
+}
+
+impl From<PartiallyDenseAdjacencyMatrixGraph> for DOT {
+    fn from(g: PartiallyDenseAdjacencyMatrixGraph) -> Self {
+        // Set graph type.
+        let graph_type = "digraph".into();
+        // Construct the vertex set.
+        let vertices = V!(g)
+            .map(|x| g.label(x).into())
+            .map(Vertex::new)
+            .map(|x| (x.id.clone(), x))
+            .collect();
+        // Construct the undirected edge set.
+        let mut undirected_arrowhead = EdgeAttributes::default();
+        undirected_arrowhead.insert_raw_parts("dir", "none");
+        let mut edges: BTreeMap<_,_> = uE!(g)
+            .map(|(x, y)| (g.label(x).into(), g.label(y).into()))
+            .map(|(x, y)| Edge{id: (x, y), op: "->".into(), attributes: undirected_arrowhead.clone()})
+            .map(|x| (x.id.clone(), x))
+            .collect();
+        // Construct the directed edge set.
+        let mut directed_edges: BTreeMap<_,_> = dE!(g)
+            .map(|(x, y)| (g.label(x).into(), g.label(y).into()))
+            .map(|(x, y)| Edge::new((x, y), "->".into()))
+            .map(|x| (x.id.clone(), x))
+            .collect();
+        // Append undirected and directed edges sets
+        edges.append(&mut directed_edges);
 
         Self {
             graph_type,
