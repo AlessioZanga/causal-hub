bec7ada71e650ae0776563a8472edc1dd41a9ea6
Niccolo-Rocchi <nik9991u@gmail.com>
2023-06-11 00:13:14 +0200

    Add parallel skeleton discovery

--- src/discovery/pc_stable.rs
+++ src/discovery/pc_stable.rs
@@ -1,6 +1,7 @@
 use std::collections::{BTreeMap, BTreeSet};
 
 use itertools::Itertools;
+use rayon::prelude::*;
 
 use crate::prelude::*;
 
@@ -53,9 +54,8 @@ where
                     if self.test.call(x, y, &z) {
                         // ... remove the edge
                         e_prime.push((x, y));
-                        //
-                        let z: BTreeSet<_> = z.into_iter().collect();
                         // Collect `(x, y)` separation set
+                        let z: BTreeSet<_> = z.into_iter().collect();
                         sepsets.insert((x, y), z.clone());
                         sepsets.insert((y, x), z);
                         // Change edge
@@ -73,10 +73,97 @@ where
         (g, sepsets)
     }
 
+    /// Private function. It performs parallel skeleton discovery given a test.
+    #[inline]
+    fn skeleton_parallel(&self) -> (Graph, SepSets) {
+        // Set complete graph
+        let mut g = Graph::complete(self.test.labels());
+        // Initialize set of separating sets
+        let mut sepsets: SepSets = BTreeMap::new();
+        // Initialize size of conditioning set
+        let mut c = 0;
+
+        'a: loop {
+            // Map and collect each edge in:
+            // 1. The edge
+            // 2. Its separation set (if any)
+            // 3. A flag indicating if exists at least one set of adjacents with cardinality `c`
+            let to_be_removed: Vec<(usize, usize, Option<BTreeSet<usize>>, bool)> = E!(g)
+                .collect::<Vec<(usize, usize)>>()
+                .into_par_iter()
+                .map(|(x, y)| {
+                    // Take superset of adjacents with cardinality `c`
+                    let adj: Vec<_> = iter_set::union(
+                        Adj!(g, x).filter(|&v| v != y).combinations(c),
+                        Adj!(g, y).filter(|&v| v != x).combinations(c),
+                    )
+                    .collect();
+
+                    // Assign each subset a flag indicating if it d-separates `(x, y)`
+                    let sepset_results: Vec<(Vec<usize>, bool)> = adj
+                        .into_par_iter()
+                        .map(|z| match self.test.call(x, y, &z) {
+                            // If such set d-separates `(x, y)` return (x, y) and its sepset
+                            true => (z, true),
+                            _ => (z, false),
+                        })
+                        .collect();
+
+                    let mut edge_flag = true;
+                    if sepset_results == vec![] {
+                        edge_flag = false
+                    }
+
+                    // Take first separating subset, if any
+                    let sepset = sepset_results
+                        .into_par_iter()
+                        .find_map_first(|(z, sep_flag)| match sep_flag {
+                            true => {
+                                let z: BTreeSet<usize> = z.into_iter().collect();
+                                Some(z)
+                            }
+                            _ => None,
+                        });
+
+                    (x, y, sepset, edge_flag)
+                })
+                .collect();
+
+            // If there are no adjacents with cardinality `c`, then break the iteration
+            if to_be_removed
+                .par_iter()
+                .all(|(_, _, _, edge_flag)| edge_flag == &false)
+            {
+                break 'a;
+            }
+
+            // Remove d-separated edges of current iteration and collect separation set
+            for (x, y, z, _) in to_be_removed {
+                match z {
+                    Some(z) => {
+                        sepsets.insert((x, y), z.clone());
+                        sepsets.insert((y, x), z);
+                        g.del_edge_by_index(x, y);
+                    }
+                    _ => continue,
+                }
+            }
+            // Increase size of conditioning set
+            c += 1;
+        }
+        (g, sepsets)
+    }
+
     /// Perform skeleton discovery given test.
     #[inline]
     pub fn call_skeleton(&self) -> Graph {
         self.skeleton().0
+    }
+
+    /// Perform parallel skeleton discovery given test.
+    #[inline]
+    pub fn call_skeleton_parallel(&self) -> Graph {
+        self.skeleton_parallel().0
     }
 
     /// Perform discovery given a test.
@@ -85,6 +172,43 @@ where
     pub fn call(&self) -> PDGraph {
         // Perform skeleton discovery
         let (g, sepsets) = self.skeleton();
+        // Cast it to a partially directed graph
+        let mut g: PDGraph = g.into();
+        // Create the set of unshielded triples
+        let mut triples: BTreeSet<(usize, usize, usize)> = BTreeSet::new();
+        for y in V!(g) {
+            for (x, z) in Adj!(g, y)
+                .combinations(2)
+                .map(|xz| (xz[0], xz[1]))
+                .filter(|(x, z)| !g.has_edge_by_index(*x, *z))
+            {
+                triples.insert((x, y, z));
+            }
+        }
+
+        // For every unshielded triple ...
+        for (x, y, z) in triples.into_iter() {
+            // ... if `y` doesn't d-separates `(x, y)` ...
+            if !sepsets[&(x, z)].contains(&y) {
+                // and both edges are undirected ...
+                if !g.has_undirected_edge_by_index(x, y) || !g.has_undirected_edge_by_index(y, z) {
+                    continue;
+                }
+                // ... then the triple is a v-structure
+                g.orient_edge(x, y);
+                g.orient_edge(z, y);
+            }
+        }
+        g
+    }
+
+    /// Perform parallel discovery given a test.
+    /// Firstly, it performs parallel skeleton discovery and then orients v-structures leveraging discovery implied separation sets.
+    #[inline]
+    pub fn call_parallel(&self) -> PDGraph {
+        //TODO: parallelize after `.skeleton_parallel()`
+        // Perform skeleton discovery
+        let (g, sepsets) = self.skeleton_parallel();
         // Cast it to a partially directed graph
         let mut g: PDGraph = g.into();
         // Create the set of unshielded triples
