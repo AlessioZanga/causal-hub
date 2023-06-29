c6ebb5b237bbaac68371e49018958b0853403206
AlessioZanga <alessio.zanga@outlook.it>
2023-06-29 15:03:39 +0200

    Add benches for scores

--- benches/main.rs
+++ benches/main.rs
@@ -1,25 +1,40 @@
 use criterion::{criterion_group, criterion_main};
 
-include!("mod.rs");
+mod data;
+mod discovery;
+mod stats;
 
 criterion_group!(
     benches,
     // Data set benchmarks.
-    crate::data::data_matrix::sample::asia,
-    crate::data::data_matrix::sample::alarm,
-    crate::data::data_matrix::sample_with_replacement::asia,
-    crate::data::data_matrix::sample_with_replacement::alarm,
+    data::data_matrix::sample::asia,
+    data::data_matrix::sample::alarm,
+    data::data_matrix::sample_with_replacement::asia,
+    data::data_matrix::sample_with_replacement::alarm,
     // Causal Discovery benchmarks.
-    crate::discovery::pc_stable::cancer,
-    crate::discovery::pc_stable::par_cancer,
-    crate::discovery::pc_stable::asia,
-    crate::discovery::pc_stable::par_asia,
-    crate::discovery::pc_stable::survey,
-    crate::discovery::pc_stable::par_survey,
-    crate::discovery::hill_climbing::call::asia,
-    crate::discovery::hill_climbing::call::alarm,
-    crate::discovery::hill_climbing::par_call::asia,
-    crate::discovery::hill_climbing::par_call::alarm,
+    discovery::pc_stable::cancer,
+    discovery::pc_stable::par_cancer,
+    discovery::pc_stable::asia,
+    discovery::pc_stable::par_asia,
+    discovery::pc_stable::survey,
+    discovery::pc_stable::par_survey,
+    discovery::hill_climbing::call::asia,
+    discovery::hill_climbing::call::alarm,
+    discovery::hill_climbing::par_call::asia,
+    discovery::hill_climbing::par_call::alarm,
+    // Statistics.
+    stats::log_likelihood::marginal::asia,
+    stats::log_likelihood::marginal::alarm,
+    stats::log_likelihood::par_marginal::asia,
+    stats::log_likelihood::par_marginal::alarm,
+    stats::akaike_information_criterion::call::asia,
+    stats::akaike_information_criterion::call::alarm,
+    stats::akaike_information_criterion::par_call::asia,
+    stats::akaike_information_criterion::par_call::alarm,
+    stats::bayesian_information_criterion::call::asia,
+    stats::bayesian_information_criterion::call::alarm,
+    stats::bayesian_information_criterion::par_call::asia,
+    stats::bayesian_information_criterion::par_call::alarm,
 );
 
 criterion_main!(benches);
---
+++ benches/stats/mod.rs
@@ -0,0 +1,3 @@
+pub mod akaike_information_criterion;
+pub mod bayesian_information_criterion;
+pub mod log_likelihood;
