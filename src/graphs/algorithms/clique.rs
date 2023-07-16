996e471121e42c6f2c6e2772b55f95fb6f1a3e27
AlessioZanga <alessio.zanga@outlook.it>
2023-07-16 17:04:17 +0200

    Initial implementation of parallel `BK`

--- src/graphs/algorithms/clique.rs
+++ src/graphs/algorithms/clique.rs
@@ -1,20 +1,26 @@
 use is_sorted::IsSorted;
+use itertools::Itertools;
+use rayon::prelude::*;
 
 use crate::{
     graphs::{directions, UndirectedGraph},
     Ne, V,
 };
 
-/// Bron-Kerbosh (BK) algorithm with pivoting.
-pub struct BronKerboschWithPivoting<'a, G> {
+/// Bron-Kerbosh (BK) algorithm.
+///
+/// Refer to
+/// ["Accelerating the Bron-Kerbosch Algorithm for Maximal Clique Enumeration Using GPUs"](https://doi.org/10.1109/TPDS.2021.3067053).
+///
+pub struct BronKerbosch<'a, G> {
     g: &'a G,
 }
 
-impl<'a, G> BronKerboschWithPivoting<'a, G>
+impl<'a, G> BronKerbosch<'a, G>
 where
     G: UndirectedGraph<Direction = directions::Undirected>,
 {
-    /// Construct a new Bron-Kerbosch Max Clique with pivoting functor.
+    /// Construct a new Bron-Kerbosch functor.
     pub const fn new(g: &'a G) -> Self {
         Self { g }
     }
@@ -22,7 +28,21 @@ where
     /// Find max-cliques of the given graph G.
     #[inline]
     pub fn call(&self) -> Vec<Vec<usize>> {
-        self.eval(vec![], V!(self.g).collect(), vec![])
+        // FIXME: Get the degeneracy order of G.
+        let v = V!(self.g).collect_vec();
+
+        v.iter()
+            .enumerate()
+            .flat_map(|(i, &v_i)| {
+                // Compute Ne(g, v_i).
+                let ne_v_i = Ne!(self.g, v_i).collect_vec();
+                // Compute V[(i + 1)..|V|] \cap Ne(g, v_i) and V[0..i] \cap Ne(g, v_i).
+                let p_i = iter_set::intersection(&v[(i + 1)..], &ne_v_i);
+                let x_i = iter_set::intersection(&v[0..i], &ne_v_i);
+
+                self.eval(vec![v_i], p_i.copied().collect(), x_i.copied().collect())
+            })
+            .collect()
     }
 
     /// Recursive call of Bron-Kerbosh algorithm.
@@ -32,11 +52,77 @@ where
         debug_assert!(p.iter().is_sorted());
         debug_assert!(x.iter().is_sorted());
 
-        // If G is null ...
-        if self.g.order() == 0 {
-            // ... return.
-            return vec![];
+        // If P and X are empty ...
+        if p.is_empty() && x.is_empty() {
+            // ... return R.
+            return vec![r];
         }
+
+        // Choose a pivot vertex u in P \cup X with maximum |P \cap Ne(g, v)|.
+        let u = *iter_set::union(&p, &x)
+            .max_by_key(|&&v| {
+                let ne_v = Ne!(self.g, v).collect_vec();
+                iter_set::intersection(&p, &ne_v).count()
+            })
+            .unwrap();
+        // Compute Ne(g, u).
+        let ne_u = Ne!(self.g, u).collect_vec();
+        // Compute P \ Ne(g, u).
+        let q = iter_set::difference(&p, &ne_u).copied().collect_vec();
+
+        // For each v in P \ Ne(g, u);
+        q.iter()
+            .flat_map(|v| {
+                // Compute Ne(g, v).
+                let ne_v = Ne!(self.g, *v).collect_vec();
+
+                // Compute R \cup {v}, P \cap Ne(g, v) and X \cap Ne(g, v).
+                let r_prime = iter_set::union(&r, [v]);
+                let p_prime = iter_set::intersection(&p, &ne_v);
+                let x_prime = iter_set::intersection(&x, &ne_v);
+
+                // Recursive call on R \cup {v}, P \cap Ne(g, v) and X \cap Ne(g, v).
+                let q = self.eval(
+                    r_prime.copied().collect(),
+                    p_prime.copied().collect(),
+                    x_prime.copied().collect(),
+                );
+
+                // Compute P \ {v} and X \cup {v}.
+                p.remove(p.binary_search(v).unwrap());
+                x.insert(x.binary_search(v).unwrap_err(), *v);
+
+                q
+            })
+            .collect()
+    }
+
+    /// Find max-cliques of the given graph G in parallel.
+    #[inline]
+    pub fn par_call(&self) -> Vec<Vec<usize>> {
+        // FIXME: Get the degeneracy order of G.
+        let v = V!(self.g).collect_vec();
+
+        v.par_iter()
+            .enumerate()
+            .flat_map(|(i, &v_i)| {
+                // Compute Ne(g, v_i).
+                let ne_v_i = Ne!(self.g, v_i).collect_vec();
+                // Compute V[(i + 1)..|V|] \cap Ne(g, v_i) and V[0..i] \cap Ne(g, v_i).
+                let p_i = iter_set::intersection(&v[(i + 1)..], &ne_v_i);
+                let x_i = iter_set::intersection(&v[0..i], &ne_v_i);
+
+                self.par_eval(vec![v_i], p_i.copied().collect(), x_i.copied().collect())
+            })
+            .collect()
+    }
+
+    /// Recursive call of Bron-Kerbosh algorithm in parallel.
+    fn par_eval(&self, r: Vec<usize>, p: Vec<usize>, x: Vec<usize>) -> Vec<Vec<usize>> {
+        // Assert R, P and X are sorted.
+        debug_assert!(r.iter().is_sorted());
+        debug_assert!(p.iter().is_sorted());
+        debug_assert!(x.iter().is_sorted());
 
         // If P and X are empty ...
         if p.is_empty() && x.is_empty() {
@@ -44,32 +130,44 @@ where
             return vec![r];
         }
 
-        // Choose a pivot vertex u in P \cup X with maximum degree.
+        // Choose a pivot vertex u in P \cup X with maximum |P \cap Ne(g, v)|.
         let u = *iter_set::union(&p, &x)
-            .max_by_key(|&&y| self.g.get_degree_by_index(y))
+            .max_by_key(|&&v| {
+                let ne_v = Ne!(self.g, v).collect_vec();
+                iter_set::intersection(&p, &ne_v).count()
+            })
             .unwrap();
+        // Compute Ne(g, u).
+        let ne_u = Ne!(self.g, u).collect_vec();
+        // Compute P \ Ne(g, u).
+        let q = iter_set::difference(&p, &ne_u).copied().collect_vec();
 
-        // Initialize the results.
-        let mut q = vec![];
+        // For each v in Q ...
+        q.par_iter()
+            .enumerate()
+            .flat_map(|(i, v_i)| {
+                // Compute Ne(g, u) \cup Q[(i + 1)..|Q|] and X \cup Q[0..i].
+                let p_i = iter_set::union(&ne_u, &q[(i + 1)..]);
+                let x_i = iter_set::union(&x, &q[0..i]);
 
-        // For each v in P \ Ne(g, u);
-        for v in iter_set::difference(p.clone(), Ne!(self.g, u)) {
-            // Compute R \cup {v}, P \cap Ne(g, v) and X \cap Ne(g, v).
-            let r_prime = iter_set::union(r.clone(), [v]).collect();
-            let p_prime = iter_set::intersection(p.clone(), Ne!(self.g, v)).collect();
-            let x_prime = iter_set::intersection(x.clone(), Ne!(self.g, v)).collect();
+                // Compute Ne(g, v).
+                let ne_v_i = Ne!(self.g, *v_i).collect_vec();
 
-            // Recursive call on R \cup {v}, P \cap Ne(g, v) and X \cap Ne(g, v).
-            q.extend(self.eval(r_prime, p_prime, x_prime));
+                // Compute R \cup {v}, P \cap Ne(g, v) and X \cap Ne(g, v).
+                let r_prime = iter_set::union(&r, [v_i]);
+                let p_prime = iter_set::intersection(p_i, &ne_v_i);
+                let x_prime = iter_set::intersection(x_i, &ne_v_i);
 
-            // Compute P \ {v} and X \cup {v}.
-            p.remove(p.binary_search(&v).unwrap());
-            x.insert(x.binary_search(&v).unwrap_err(), v);
-        }
-
-        q
+                // Recursive call on R \cup {v}, P \cap Ne(g, v) and X \cap Ne(g, v).
+                self.par_eval(
+                    r_prime.copied().collect(),
+                    p_prime.copied().collect(),
+                    x_prime.copied().collect(),
+                )
+            })
+            .collect()
     }
 }
 
 /// Alias for the Bron-Kerbosh algorithm.
-pub type BK<'a, G> = BronKerboschWithPivoting<'a, G>;
+pub type BK<'a, G> = BronKerbosch<'a, G>;
--- tests/graphs/algorithms/clique.rs
+++ tests/graphs/algorithms/clique.rs
@@ -51,6 +51,54 @@ mod tests {
                     assert_eq!(pred_c, true_c);
                 }
             }
+
+            #[test]
+            fn par_call() {
+                let data = vec![
+                    (vec![], vec![], vec![]),
+                    (vec!["A"], vec![], vec![vec![0]]),
+                    (vec!["A", "B"], vec![], vec![vec![0], vec![1]]),
+                    (vec!["A", "B"], vec![("A", "B")], vec![vec![0, 1]]),
+                    (
+                        vec!["A", "B", "C"],
+                        vec![("A", "B")],
+                        vec![vec![0, 1], vec![2]],
+                    ),
+                    (
+                        vec!["A", "B", "C"],
+                        vec![("A", "B"), ("B", "C")],
+                        vec![vec![0, 1], vec![1, 2]],
+                    ),
+                    (
+                        vec!["A", "B", "C"],
+                        vec![("A", "B"), ("B", "C"), ("C", "A")],
+                        vec![vec![0, 1, 2]],
+                    ),
+                    (
+                        vec!["A", "B", "C", "D"],
+                        vec![("A", "B"), ("B", "C"), ("C", "A")],
+                        vec![vec![0, 1, 2], vec![3]],
+                    ),
+                    (
+                        vec!["A", "B", "C", "D"],
+                        vec![("A", "B"), ("B", "C"), ("C", "A"), ("C", "D")],
+                        vec![vec![0, 1, 2], vec![2, 3]],
+                    ),
+                    (
+                        vec!["A", "B", "C", "D"],
+                        vec![("A", "B"), ("B", "C"), ("C", "A"), ("C", "D"), ("D", "B")],
+                        vec![vec![0, 1, 2], vec![1, 2, 3]],
+                    ),
+                ];
+
+                for (v, e, true_c) in data {
+                    let g = $G::new(v, e);
+
+                    let pred_c = BK::new(&g).par_call();
+
+                    assert_eq!(pred_c, true_c);
+                }
+            }
         };
     }
 
