5b27ff1957bfc962f2656f7d66986d3b9669f911
AlessioZanga <alessio.zanga@outlook.it>
2025-03-19 21:16:54 +0100

    Update categorical distribution

--- src/categorical.rs
+++ src/categorical.rs
@@ -1,32 +1,86 @@
 use ndarray::Array2;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+/// A struct representing a categorical distribution.
+///
 pub struct Categorical {
-    variables: Vec<(String, Vec<String>)>,
+    labels: Vec<String>,
+    states: Vec<Vec<String>>,
     probabilities: Array2<f64>,
 }
 
 impl Categorical {
-    pub fn new(
-        variables: Vec<(String, Vec<String>)>,
-        probabilities: Array2<f64>,
-    ) -> Self {
+    /// Creates a new categorical distribution.
+    ///
+    /// # Arguments
+    ///
+    /// * `variables` - The variables and their states.
+    /// * `probabilities` - The probabilities of the states.
+    ///
+    /// # Returns
+    ///
+    /// A new `Categorical` instance.
+    ///
+    pub fn new(variables: &[(&str, Vec<&str>)], probabilities: Array2<f64>) -> Self {
+        // Convert the array of string slices to a vector of strings.
+        let (labels, states): (Vec<_>, Vec<Vec<_>>) = {
+            (
+                // Get the labels of the variables.
+                variables.iter().map(|(i, _)| i.to_string()).collect(),
+                // Get the states of the variables.
+                variables
+                    .iter()
+                    .map(|(_, i)| i.iter().map(|s| s.to_string()).collect())
+                    .collect(),
+            )
+        };
+
+        // Check if the number of states of the first variable matches the number of columns.
+        assert_eq!(
+            probabilities.ncols(),
+            states.get(0).map(|i| i.len()).unwrap_or(0),
+            "Number of states of the first variable does not match the number of columns."
+        );
+        // Check if the product of the number of states of the remaining variables matches the number of rows.
+        assert_eq!(
+            probabilities.nrows(),
+            states.iter().skip(1).map(|i| i.len()).product(),
+            "Product of the number of states of the remaining variables does not match the number of rows."
+        );
+
         Self {
-            variables,
+            labels,
+            states,
             probabilities,
         }
     }
 
-    pub fn get_probability(&self, state: Vec<usize>) -> f64 {
-        let mut index = 0;
-        let mut multiplier = 1;
+    /// Returns the labels of the variables in the categorical distribution.
+    /// 
+    /// # Returns
+    /// 
+    /// A reference to the vector of labels.
+    /// 
+    pub fn labels(&self) -> &Vec<String> {
+        &self.labels
+    }
 
-        for (i, &s) in state.iter().rev().enumerate() {
-            index += s * multiplier;
-            multiplier *= self.variables[i].1.len();
-        }
+    /// Returns the states of the variables in the categorical distribution.
+    /// 
+    /// # Returns
+    /// 
+    /// A reference to the vector of states.
+    /// 
+    pub fn states(&self) -> &Vec<Vec<String>> {
+        &self.states
+    }
 
-        self.probabilities[[index / self.variables[0].1.len(), index % self.variables[0].1.len()]]
+    /// Returns the probabilities of the states in the categorical distribution.
+    /// 
+    /// # Returns
+    /// 
+    /// A reference to the array of probabilities.
+    /// 
+    pub fn probabilities(&self) -> &Array2<f64> {
+        &self.probabilities
     }
 }
--- tests/categorical.rs
+++ tests/categorical.rs
@@ -6,19 +6,11 @@ mod tests {
     #[test]
     fn test_get_probability() {
         let variables = vec![
-            ("A".to_string(), vec!["a1".to_string(), "a2".to_string()]),
-            ("B".to_string(), vec!["b1".to_string(), "b2".to_string()]),
+            ("A", vec!["no", "yes"]),
+            ("B", vec!["no", "yes"]),
+            ("C", vec!["no", "yes"]),
         ];
         let probabilities = array![[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]];
-        let categorical = Categorical::new(variables, probabilities);
-
-        assert_eq!(categorical.get_probability(vec![0, 0]), 0.1);
-        assert_eq!(categorical.get_probability(vec![0, 1]), 0.9);
-        assert_eq!(categorical.get_probability(vec![1, 0]), 0.2);
-        assert_eq!(categorical.get_probability(vec![1, 1]), 0.8);
-        assert_eq!(categorical.get_probability(vec![2, 0]), 0.3);
-        assert_eq!(categorical.get_probability(vec![2, 1]), 0.7);
-        assert_eq!(categorical.get_probability(vec![3, 0]), 0.4);
-        assert_eq!(categorical.get_probability(vec![3, 1]), 0.6);
+        let categorical = Categorical::new(&variables, probabilities);
     }
 }
