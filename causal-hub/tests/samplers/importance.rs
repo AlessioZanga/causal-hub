3ee6f65ecea0fa6964e6dc52fe7264bdc2385724
AlessioZanga <alessio.zanga@outlook.it>
2025-05-12 23:06:08 +0200

    Update tests

--- causal-hub/tests/samplers/importance.rs
+++ causal-hub/tests/samplers/importance.rs
@@ -9,6 +9,7 @@ mod tests {
             models::CTBN,
             samplers::ImportanceSampler,
         };
+        use ndarray::prelude::*;
         use rand::SeedableRng;
         use rand_xoshiro::Xoshiro256PlusPlus;
 
@@ -22,14 +23,40 @@ mod tests {
             // Initialize evidence.
             let evidence = CategoricalTrjEv::new(
                 ctbn.states(),
-                [(
-                    "Hungry",
-                    E::CertainPositiveInterval {
-                        state: 1,
-                        start_time: 0.,
-                        end_time: 1.,
-                    },
-                )],
+                [
+                    (
+                        "Hungry",
+                        E::CertainPositiveInterval {
+                            state: 1,
+                            start_time: 0.,
+                            end_time: 0.2,
+                        },
+                    ),
+                    (
+                        "Eating",
+                        E::CertainNegativeInterval {
+                            not_states: [0].into_iter().collect(),
+                            start_time: 0.,
+                            end_time: 0.2,
+                        },
+                    ),
+                    (
+                        "FullStomach",
+                        E::UncertainPositiveInterval {
+                            p_states: array![0.3, 0.7],
+                            start_time: 0.1,
+                            end_time: 0.2,
+                        },
+                    ),
+                    (
+                        "Hungry",
+                        E::UncertainNegativeInterval {
+                            p_not_states: array![0.1, 0.9],
+                            start_time: 0.3,
+                            end_time: 0.5,
+                        },
+                    ),
+                ],
             );
 
             // Initialize sampler.
