2e5cede91f5c7491c53cfe3a20ad3027b1c66d54
AlessioZanga <alessio.zanga@outlook.it>
2025-06-19 14:40:30 +0200

    Fix parallelism inhibition by GIL

--- python/src/estimators/structures/structural_expectation_maximization.rs
+++ python/src/estimators/structures/structural_expectation_maximization.rs
@@ -56,150 +56,153 @@ pub fn sem(
         .and_then(|x| x.extract(py).ok())
         .unwrap_or_else(|| 0.01);
 
-    // Initialize the random number generator.
-    let mut rng = Xoshiro256PlusPlus::seed_from_u64(seed);
+    // Release the GIL to allow parallel execution.
+    py.allow_threads(|| {
+        // Initialize the random number generator.
+        let mut rng = Xoshiro256PlusPlus::seed_from_u64(seed);
 
-    // Set the initial graph depending on the algorithm.
-    let initial_graph = match algorithm {
-        "ctpc" => {
-            // Log the graph initialization.
-            debug!("Setting initial graph for CTPC algorithm to a complete graph ...");
-            // Set the initial graph to a complete graph.
-            let mut initial_graph = DiGraph::complete(evidence.labels());
-            // Check if the number of vertices is less than or equal to the maximum number of parents.
-            if initial_graph.vertices().len() > max_parents + 1 {
-                // Log the maximum number of parents.
-                debug!("Reducing the number of parents to {max_parents}.");
-                // Set the parents of the initial graph to max_parents.
-                for i in 0..initial_graph.vertices().len() {
-                    // Get the parents.
-                    let mut pa_i = initial_graph.parents(i);
-                    // Choose nodes randomly.
-                    pa_i.shuffle(&mut rng);
-                    // Remove the excess parents.
-                    for j in pa_i.split_off(max_parents) {
-                        // Remove the edge.
-                        initial_graph.del_edge(j, i);
+        // Set the initial graph depending on the algorithm.
+        let initial_graph = match algorithm {
+            "ctpc" => {
+                // Log the graph initialization.
+                debug!("Setting initial graph for CTPC algorithm to a complete graph ...");
+                // Set the initial graph to a complete graph.
+                let mut initial_graph = DiGraph::complete(evidence.labels());
+                // Check if the number of vertices is less than or equal to the maximum number of parents.
+                if initial_graph.vertices().len() > max_parents + 1 {
+                    // Log the maximum number of parents.
+                    debug!("Reducing the number of parents to {max_parents}.");
+                    // Set the parents of the initial graph to max_parents.
+                    for i in 0..initial_graph.vertices().len() {
+                        // Get the parents.
+                        let mut pa_i = initial_graph.parents(i);
+                        // Choose nodes randomly.
+                        pa_i.shuffle(&mut rng);
+                        // Remove the excess parents.
+                        for j in pa_i.split_off(max_parents) {
+                            // Remove the edge.
+                            initial_graph.del_edge(j, i);
+                        }
                     }
                 }
-            }
-            // Return the initial graph.
-            initial_graph
-        }
-        "cthc" => {
-            // Log the graph initialization.
-            debug!("Setting initial graph for CTHC algorithm to an empty graph ...");
-            // Set the initial graph to an empty graph.
-            DiGraph::empty(evidence.labels())
-        }
-        _ => panic!(
-            "Failed to get the structure learning algorithm: \n\
-            \t expected:   'ctpc' or 'cthc', \n\
-            \t found:      '{algorithm}'"
-        ),
-    };
-
-    // Log the raw estimator initialization.
-    debug!("Initializing the raw estimator for the initial guess ...");
-    // Initialize a raw estimator for an initial guess.
-    let raw = RAWE::<'_, _, CatTrjsEv, CatTrjs>::par_new(&mut rng, &evidence);
-    // Log the initial model fitting.
-    debug!("Fitting the initial model using the raw estimator ...");
-    // Set the initial model.
-    let initial_model = raw.par_fit(initial_graph.clone());
-
-    // Wrap the random number generator in a RefCell to allow mutable borrowing.
-    let rng = RefCell::new(rng);
-
-    // Define the expectation step.
-    let e_step = |prev_model: &CatCTBN, evidence: &CatTrjsEv| -> CatWtdTrjs {
-        // Reference the random number generator.
-        let mut rng = rng.borrow_mut();
-        // Sample the seeds to parallelize the sampling.
-        let seeds: Vec<_> = (0..evidence.values().len())
-            .map(|_| rng.next_u64())
-            .collect();
-        // Get the max length of the evidence.
-        let max_length = evidence
-            .values()
-            .iter()
-            .map(|e| e.sample_size())
-            .max()
-            .unwrap_or(10);
-        // Fore each (seed, evidence) ...
-        seeds
-            .iter()
-            .zip(evidence)
-            .par_bridge()
-            .map(|(&s, e)| {
-                // Initialize a new random number generator.
-                let mut rng = Xoshiro256PlusPlus::seed_from_u64(s);
-                // Initialize a new sampler.
-                let mut importance = ImportanceSampler::new(&mut rng, prev_model, e);
-                // Perform multiple imputation.
-                let trjs = importance.sample_n_by_length(max_length, 10);
-                // Get the one with the highest weight.
-                trjs.values()
-                    .iter()
-                    .max_by(|a, b| a.weight().partial_cmp(&b.weight()).unwrap())
-                    .unwrap()
-                    .clone()
-            })
-            .collect()
-    };
-
-    // Define the maximization step.
-    let m_step = |_prev_model: &CatCTBN, expectation: &CatWtdTrjs| -> CatCTBN {
-        // Initialize the parameter estimator.
-        let estimator = BE::new(expectation, (1, 1.));
-        // Cache the parameter estimator.
-        let cache = Cache::new(&estimator);
-        // Learn the graph.
-        let fitted_graph = match algorithm {
-            "ctpc" => {
-                // Initialize the F test.
-                let f_test = FTest::new(&cache, f_test);
-                // Initialize the chi-squared test.
-                let chi_sq_test = ChiSquaredTest::new(&cache, c_test);
-                // Initialize the CTPC algorithm.
-                let ctpc = CTPC::new(&initial_graph, &f_test, &chi_sq_test);
-                // Fit the new structure using CTPC.
-                ctpc.par_fit()
+                // Return the initial graph.
+                initial_graph
             }
             "cthc" => {
-                // Initialize the scoring criterion.
-                let bic = BIC::new(&cache);
-                // Initialize the CTHC algorithm and set the maximum number of parents.
-                let cthc = CTHC::new(&initial_graph, &bic).with_max_parents(max_parents);
-                // Fit the new structure using CTHC.
-                cthc.par_fit()
+                // Log the graph initialization.
+                debug!("Setting initial graph for CTHC algorithm to an empty graph ...");
+                // Set the initial graph to an empty graph.
+                DiGraph::empty(evidence.labels())
             }
             _ => panic!(
                 "Failed to get the structure learning algorithm: \n\
-                \t expected:   'ctpc' or 'cthc', \n\
-                \t found:      '{algorithm}'"
+            \t expected:   'ctpc' or 'cthc', \n\
+            \t found:      '{algorithm}'"
             ),
         };
-        // Fit the new model using the expectation.
-        estimator.par_fit(fitted_graph)
-    };
 
-    // Define the stopping criteria.
-    let stop = |prev_model: &CatCTBN, curr_model: &CatCTBN, counter: usize| -> bool {
-        // Check if the models are equal or the counter is greater than the limit.
-        relative_eq!(prev_model, curr_model, epsilon = 5e-2) || counter >= max_iter
-    };
+        // Log the raw estimator initialization.
+        debug!("Initializing the raw estimator for the initial guess ...");
+        // Initialize a raw estimator for an initial guess.
+        let raw = RAWE::<'_, _, CatTrjsEv, CatTrjs>::par_new(&mut rng, &evidence);
+        // Log the initial model fitting.
+        debug!("Fitting the initial model using the raw estimator ...");
+        // Set the initial model.
+        let initial_model = raw.par_fit(initial_graph.clone());
 
-    // Create a new builder.
-    let builder = EMBuilder::new(&initial_model, evidence)
-        .with_e_step(&e_step)
-        .with_m_step(&m_step)
-        .with_stop(&stop)
-        .build();
+        // Wrap the random number generator in a RefCell to allow mutable borrowing.
+        let rng = RefCell::new(rng);
 
-    // Fit the model.
-    let fitted_model = builder.fit();
+        // Define the expectation step.
+        let e_step = |prev_model: &CatCTBN, evidence: &CatTrjsEv| -> CatWtdTrjs {
+            // Reference the random number generator.
+            let mut rng = rng.borrow_mut();
+            // Sample the seeds to parallelize the sampling.
+            let seeds: Vec<_> = (0..evidence.values().len())
+                .map(|_| rng.next_u64())
+                .collect();
+            // Get the max length of the evidence.
+            let max_length = evidence
+                .values()
+                .iter()
+                .map(|e| e.sample_size())
+                .max()
+                .unwrap_or(10);
+            // Fore each (seed, evidence) ...
+            seeds
+                .iter()
+                .zip(evidence)
+                .par_bridge()
+                .map(|(&s, e)| {
+                    // Initialize a new random number generator.
+                    let mut rng = Xoshiro256PlusPlus::seed_from_u64(s);
+                    // Initialize a new sampler.
+                    let mut importance = ImportanceSampler::new(&mut rng, prev_model, e);
+                    // Perform multiple imputation.
+                    let trjs = importance.sample_n_by_length(max_length, 10);
+                    // Get the one with the highest weight.
+                    trjs.values()
+                        .iter()
+                        .max_by(|a, b| a.weight().partial_cmp(&b.weight()).unwrap())
+                        .unwrap()
+                        .clone()
+                })
+                .collect()
+        };
 
-    // Convert the fitted model into a PyDiGraph.
-    Ok(fitted_model.into())
+        // Define the maximization step.
+        let m_step = |_prev_model: &CatCTBN, expectation: &CatWtdTrjs| -> CatCTBN {
+            // Initialize the parameter estimator.
+            let estimator = BE::new(expectation, (1, 1.));
+            // Cache the parameter estimator.
+            let cache = Cache::new(&estimator);
+            // Learn the graph.
+            let fitted_graph = match algorithm {
+                "ctpc" => {
+                    // Initialize the F test.
+                    let f_test = FTest::new(&cache, f_test);
+                    // Initialize the chi-squared test.
+                    let chi_sq_test = ChiSquaredTest::new(&cache, c_test);
+                    // Initialize the CTPC algorithm.
+                    let ctpc = CTPC::new(&initial_graph, &f_test, &chi_sq_test);
+                    // Fit the new structure using CTPC.
+                    ctpc.par_fit()
+                }
+                "cthc" => {
+                    // Initialize the scoring criterion.
+                    let bic = BIC::new(&cache);
+                    // Initialize the CTHC algorithm and set the maximum number of parents.
+                    let cthc = CTHC::new(&initial_graph, &bic).with_max_parents(max_parents);
+                    // Fit the new structure using CTHC.
+                    cthc.par_fit()
+                }
+                _ => panic!(
+                    "Failed to get the structure learning algorithm: \n\
+                \t expected:   'ctpc' or 'cthc', \n\
+                \t found:      '{algorithm}'"
+                ),
+            };
+            // Fit the new model using the expectation.
+            estimator.par_fit(fitted_graph)
+        };
+
+        // Define the stopping criteria.
+        let stop = |prev_model: &CatCTBN, curr_model: &CatCTBN, counter: usize| -> bool {
+            // Check if the models are equal or the counter is greater than the limit.
+            relative_eq!(prev_model, curr_model, epsilon = 5e-2) || counter >= max_iter
+        };
+
+        // Create a new builder.
+        let builder = EMBuilder::new(&initial_model, evidence)
+            .with_e_step(&e_step)
+            .with_m_step(&m_step)
+            .with_stop(&stop)
+            .build();
+
+        // Fit the model.
+        let fitted_model = builder.fit();
+
+        // Convert the fitted model into a PyDiGraph.
+        Ok(fitted_model.into())
+    })
 }
