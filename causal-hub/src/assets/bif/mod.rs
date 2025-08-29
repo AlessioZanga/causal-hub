766512618003ffaea709a4ed78fda3a8e23cabe2
AlessioZanga <alessio.zanga@outlook.it>
2025-08-29 13:03:55 +0200

    Update assets folder structure (2)

---
+++ causal-hub/src/assets/bif/mod.rs
@@ -0,0 +1,22 @@
+use dry::macro_for;
+use log::debug;
+use paste::paste;
+
+use crate::{io::BifParser, models::CatBN};
+
+macro_for!(
+    $bn in [
+        alarm, andes, asia, barley, cancer, child, diabetes, earthquake,
+        hailfinder, hepar2, insurance, link, mildew, munin1, pathfinder,
+        pigs, sachs, survey, water, win95pts
+    ] {
+    paste! {
+        #[doc = "Load the `" $bn:upper "` BN from the assets."]
+        pub fn [<load_ $bn>]() -> CatBN {
+            // Log the loading of the BN.
+            debug!("Loading the '{}' BN from assets.", stringify!($bn));
+            // Read the BIF file and return the BN.
+            BifParser::parse_str(include_str!(concat!(stringify!($bn), ".bif")))
+        }
+    }
+});
