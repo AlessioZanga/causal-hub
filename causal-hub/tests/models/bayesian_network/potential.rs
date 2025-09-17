c8df095eaacf3bbeb780ce6e31e28e1f087398fc
AlessioZanga <alessio.zanga@outlook.it>
2025-09-17 23:16:54 +0200

    Add scaffolding for `CatPhi` tests

---
+++ causal-hub/tests/models/bayesian_network/potential.rs
@@ -0,0 +1,52 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn new() {
+        todo!() // TODO:
+    }
+
+    #[test]
+    fn condition() {
+        todo!() // TODO:
+    }
+
+    #[test]
+    fn marginalize() {
+        todo!() // TODO:
+    }
+
+    #[test]
+    fn normalize() {
+        todo!() // TODO:
+    }
+
+    #[test]
+    fn multiply() {
+        todo!() // TODO:
+
+        // mul_assign(&mut self)
+        // mul(self)
+        // mul(&self)
+        // mul(&mut self)
+    }
+
+    #[test]
+    fn divide() {
+        todo!() // TODO:
+
+        // div_assign(&mut self)
+        // div(self)
+        // div(&self)
+        // div(&mut self)
+    }
+
+    #[test]
+    fn from_cpd() {
+        todo!() // TODO:
+    }
+
+    #[test]
+    fn into_cpd() {
+        todo!() // TODO:
+    }
+}
