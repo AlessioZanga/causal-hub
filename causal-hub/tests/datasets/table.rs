31dd26877a9550277012c130e87e85a5d2251985
AlessioZanga <alessio.zanga@outlook.it>
2025-10-07 14:20:51 +0200

    Add trajectory tests scaffolding

--- causal-hub/tests/datasets/table.rs
+++ causal-hub/tests/datasets/table.rs
@@ -9,164 +9,168 @@ mod tests {
     };
     use ndarray::prelude::*;
 
-    #[test]
-    fn new() {
-        let states = states![
-            ("B", ["no", "yes"]),
-            ("C", ["yes", "no"]),
-            ("A", ["no", "yes"]),
-        ];
-        let values = array![
-            [0, 1, 0], //
-            [0, 0, 0], //
-            [1, 0, 0], //
-            [1, 0, 1]
-        ];
-        let dataset = CatTable::new(states, values.clone());
+    mod categorical {
+        use super::*;
 
-        assert_eq!(&labels!["A", "B", "C"], dataset.labels());
-        assert!(dataset.labels().iter().is_sorted());
-        assert!(
-            dataset
-                .states()
-                .values()
-                .all(|x| x.iter().eq(["no", "yes"]))
-        );
-        assert_eq!(
-            dataset.values(),
-            &array![
+        #[test]
+        fn new() {
+            let states = states![
+                ("B", ["no", "yes"]),
+                ("C", ["yes", "no"]),
+                ("A", ["no", "yes"]),
+            ];
+            let values = array![
+                [0, 1, 0], //
                 [0, 0, 0], //
-                [0, 0, 1], //
-                [0, 1, 1], //
-                [1, 1, 1]
-            ]
-        );
-    }
+                [1, 0, 0], //
+                [1, 0, 1]
+            ];
+            let dataset = CatTable::new(states, values.clone());
 
-    #[test]
-    fn new_different_states() {
-        let states = states![
-            ("B", ["no", "yes"]),
-            ("C", ["yes", "no", "maybe"]),
-            ("A", ["no", "yes"]),
-        ];
-        let values = array![
-            [0, 1, 0], //
-            [0, 0, 0], //
-            [1, 0, 0], //
-            [1, 0, 1]
-        ];
-        let dataset = CatTable::new(states, values.clone());
+            assert_eq!(&labels!["A", "B", "C"], dataset.labels());
+            assert!(dataset.labels().iter().is_sorted());
+            assert!(
+                dataset
+                    .states()
+                    .values()
+                    .all(|x| x.iter().eq(["no", "yes"]))
+            );
+            assert_eq!(
+                dataset.values(),
+                &array![
+                    [0, 0, 0], //
+                    [0, 0, 1], //
+                    [0, 1, 1], //
+                    [1, 1, 1]
+                ]
+            );
+        }
 
-        assert_eq!(&labels!["A", "B", "C"], dataset.labels());
-        assert!(dataset.labels().iter().is_sorted());
-        assert_eq!(
-            dataset.values(),
-            &array![
-                [0, 0, 1], //
-                [0, 0, 2], //
-                [0, 1, 2], //
-                [1, 1, 2]
-            ]
-        );
-    }
+        #[test]
+        fn new_different_states() {
+            let states = states![
+                ("B", ["no", "yes"]),
+                ("C", ["yes", "no", "maybe"]),
+                ("A", ["no", "yes"]),
+            ];
+            let values = array![
+                [0, 1, 0], //
+                [0, 0, 0], //
+                [1, 0, 0], //
+                [1, 0, 1]
+            ];
+            let dataset = CatTable::new(states, values.clone());
 
-    #[test]
-    #[should_panic = "Number of variables must be equal to the number of columns: \n\t expected:    |states| == |values.columns()| , \n\t found:       |states| == 3 and |values.columns()| == 4 ."]
-    fn new_non_unique_labels() {
-        let states = states![
-            ("B", ["no", "yes"]),
-            ("C", ["yes", "no"]),
-            ("A", ["no", "yes"]),
-            ("A", ["maybe"]), // 'A' is repeated
-        ];
-        let values = array![
-            [0, 1, 0, 0], //
-            [0, 0, 0, 1], //
-            [1, 0, 0, 1], //
-            [1, 0, 1, 0]
-        ];
-        CatTable::new(states, values);
-    }
+            assert_eq!(&labels!["A", "B", "C"], dataset.labels());
+            assert!(dataset.labels().iter().is_sorted());
+            assert_eq!(
+                dataset.values(),
+                &array![
+                    [0, 0, 1], //
+                    [0, 0, 2], //
+                    [0, 1, 2], //
+                    [1, 1, 2]
+                ]
+            );
+        }
 
-    #[test]
-    #[should_panic = "Variable 'A' should have less than 256 states: \n\t expected:    |states| <  256 , \n\t found:       |states| == 256 ."]
-    fn new_too_many_states() {
-        let too_many_states: Vec<_> = (0..256).map(|i| i.to_owned()).collect();
-        let too_many_states: Set<_> = too_many_states.iter().map(|s| s.to_string()).collect();
-        let mut states = states![("B", ["no", "yes"]), ("C", ["yes", "no"]),];
-        states.insert("A".to_owned(), too_many_states);
+        #[test]
+        #[should_panic = "Number of variables must be equal to the number of columns: \n\t expected:    |states| == |values.columns()| , \n\t found:       |states| == 3 and |values.columns()| == 4 ."]
+        fn new_non_unique_labels() {
+            let states = states![
+                ("B", ["no", "yes"]),
+                ("C", ["yes", "no"]),
+                ("A", ["no", "yes"]),
+                ("A", ["maybe"]), // 'A' is repeated
+            ];
+            let values = array![
+                [0, 1, 0, 0], //
+                [0, 0, 0, 1], //
+                [1, 0, 0, 1], //
+                [1, 0, 1, 0]
+            ];
+            CatTable::new(states, values);
+        }
 
-        let values = array![
-            [0, 1, 0], //
-            [0, 0, 0], //
-            [1, 0, 0], //
-            [1, 0, 1]
-        ];
-        CatTable::new(states, values);
-    }
+        #[test]
+        #[should_panic = "Variable 'A' should have less than 256 states: \n\t expected:    |states| <  256 , \n\t found:       |states| == 256 ."]
+        fn new_too_many_states() {
+            let too_many_states: Vec<_> = (0..256).map(|i| i.to_owned()).collect();
+            let too_many_states: Set<_> = too_many_states.iter().map(|s| s.to_string()).collect();
+            let mut states = states![("B", ["no", "yes"]), ("C", ["yes", "no"]),];
+            states.insert("A".to_owned(), too_many_states);
 
-    #[test]
-    #[should_panic = "Number of variables must be equal to the number of columns: \n\t expected:    |states| == |values.columns()| , \n\t found:       |states| == 3 and |values.columns()| == 2 ."]
-    fn new_wrong_variables_and_columns() {
-        let states = states![
-            ("B", ["no", "yes"]),
-            ("C", ["yes", "no"]),
-            ("A", ["no", "yes"]),
-        ];
-        let values = array![
-            [0, 1], //
-            [0, 0], //
-            [1, 0], //
-            [1, 0]
-        ];
-        CatTable::new(states, values);
-    }
+            let values = array![
+                [0, 1, 0], //
+                [0, 0, 0], //
+                [1, 0, 0], //
+                [1, 0, 1]
+            ];
+            CatTable::new(states, values);
+        }
 
-    #[test]
-    #[should_panic = "Values of variable 'A' must be less than the number of states: \n\t expected: values[.., 'A'] < |states['A']| , \n\t found:    values[.., 'A'] == 2 and |states['A']| == 2 ."]
-    fn new_wrong_values() {
-        let states = states![
-            ("B", ["no", "yes"]),
-            ("C", ["yes", "no"]),
-            ("A", ["no", "yes"]),
-        ];
-        let values = array![
-            [0, 1, 2], // 'A' has a value of 2 which is not valid
-            [0, 0, 0],
-            [1, 0, 0],
-            [1, 0, 1]
-        ];
-        CatTable::new(states, values);
-    }
+        #[test]
+        #[should_panic = "Number of variables must be equal to the number of columns: \n\t expected:    |states| == |values.columns()| , \n\t found:       |states| == 3 and |values.columns()| == 2 ."]
+        fn new_wrong_variables_and_columns() {
+            let states = states![
+                ("B", ["no", "yes"]),
+                ("C", ["yes", "no"]),
+                ("A", ["no", "yes"]),
+            ];
+            let values = array![
+                [0, 1], //
+                [0, 0], //
+                [1, 0], //
+                [1, 0]
+            ];
+            CatTable::new(states, values);
+        }
 
-    #[test]
-    fn display() {
-        let states = states![
-            ("B", ["no", "yes"]),
-            ("C", ["yes", "no"]),
-            ("A", ["no", "yes"]),
-        ];
-        let values = array![
-            [0, 1, 0], //
-            [0, 0, 0], //
-            [1, 0, 0], //
-            [1, 0, 1]
-        ];
-        let dataset = CatTable::new(states, values);
+        #[test]
+        #[should_panic = "Values of variable 'A' must be less than the number of states: \n\t expected: values[.., 'A'] < |states['A']| , \n\t found:    values[.., 'A'] == 2 and |states['A']| == 2 ."]
+        fn new_wrong_values() {
+            let states = states![
+                ("B", ["no", "yes"]),
+                ("C", ["yes", "no"]),
+                ("A", ["no", "yes"]),
+            ];
+            let values = array![
+                [0, 1, 2], // 'A' has a value of 2 which is not valid
+                [0, 0, 0],
+                [1, 0, 0],
+                [1, 0, 1]
+            ];
+            CatTable::new(states, values);
+        }
 
-        assert_eq!(
-            dataset.to_string(),
-            concat!(
-                "-------------------\n",
-                "| A   | B   | C   |\n",
-                "| --- | --- | --- |\n",
-                "| no  | no  | no  |\n",
-                "| no  | no  | yes |\n",
-                "| no  | yes | yes |\n",
-                "| yes | yes | yes |\n",
-                "-------------------\n",
-            )
-        );
+        #[test]
+        fn display() {
+            let states = states![
+                ("B", ["no", "yes"]),
+                ("C", ["yes", "no"]),
+                ("A", ["no", "yes"]),
+            ];
+            let values = array![
+                [0, 1, 0], //
+                [0, 0, 0], //
+                [1, 0, 0], //
+                [1, 0, 1]
+            ];
+            let dataset = CatTable::new(states, values);
+
+            assert_eq!(
+                dataset.to_string(),
+                concat!(
+                    "-------------------\n",
+                    "| A   | B   | C   |\n",
+                    "| --- | --- | --- |\n",
+                    "| no  | no  | no  |\n",
+                    "| no  | no  | yes |\n",
+                    "| no  | yes | yes |\n",
+                    "| yes | yes | yes |\n",
+                    "-------------------\n",
+                )
+            );
+        }
     }
 }
--- causal-hub/tests/datasets/trajectory.rs
+++ causal-hub/tests/datasets/trajectory.rs
@@ -6,40 +6,58 @@ mod tests {
     };
     use ndarray::prelude::*;
 
-    #[test]
-    fn new_evidence() {
-        // Initialize the model.
-        let states = states![("B", ["0", "1"]), ("A", ["0", "1", "2"]), ("C", ["0", "1"]),];
+    mod categorical {
+        use super::*;
 
-        // Initialize evidence.
-        let _evidence = CatTrjEv::new(
-            states,
-            [
-                E::CertainPositiveInterval {
-                    event: 2,
-                    state: 0,
-                    start_time: 0.,
-                    end_time: 0.2,
-                },
-                E::CertainNegativeInterval {
-                    event: 0,
-                    not_states: [0].into_iter().collect(),
-                    start_time: 0.,
-                    end_time: 0.2,
-                },
-                E::UncertainPositiveInterval {
-                    event: 1,
-                    p_states: array![0.3, 0.7, 0.],
-                    start_time: 0.1,
-                    end_time: 0.2,
-                },
-                E::UncertainNegativeInterval {
-                    event: 2,
-                    p_not_states: array![0.9, 0.1],
-                    start_time: 0.3,
-                    end_time: 0.5,
-                },
-            ],
-        );
+        #[test]
+        fn new_trajectory() {
+            todo!() // FIXME:
+        }
+
+        #[test]
+        fn new_trajectories() {
+            todo!() // FIXME:
+        }
+
+        #[test]
+        fn new_evidence() {
+            // Initialize the model.
+            let states = states![
+                ("B", ["0", "1"]),      //
+                ("A", ["0", "1", "2"]), //
+                ("C", ["0", "1"])       //
+            ];
+
+            // Initialize evidence.
+            let _evidence = CatTrjEv::new(
+                states,
+                [
+                    E::CertainPositiveInterval {
+                        event: 2,
+                        state: 0,
+                        start_time: 0.,
+                        end_time: 0.2,
+                    },
+                    E::CertainNegativeInterval {
+                        event: 0,
+                        not_states: [0].into_iter().collect(),
+                        start_time: 0.,
+                        end_time: 0.2,
+                    },
+                    E::UncertainPositiveInterval {
+                        event: 1,
+                        p_states: array![0.3, 0.7, 0.],
+                        start_time: 0.1,
+                        end_time: 0.2,
+                    },
+                    E::UncertainNegativeInterval {
+                        event: 2,
+                        p_not_states: array![0.9, 0.1],
+                        start_time: 0.3,
+                        end_time: 0.5,
+                    },
+                ],
+            );
+        }
     }
 }
