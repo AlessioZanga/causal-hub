0715b808e9979bee67cb6591570448a5ece33f41
AlessioZanga <alessio.zanga@outlook.it>
2025-10-23 01:39:18 +0200

    Add scaling to `pinv` for numerical stability

--- causal-hub/src/utils/pseudo_inverse.rs
+++ causal-hub/src/utils/pseudo_inverse.rs
@@ -1,5 +1,5 @@
 use ndarray::prelude::*;
-use ndarray_linalg::SVD;
+use ndarray_linalg::SVDInto;
 use ndarray_stats::QuantileExt;
 
 use crate::types::EPSILON;
@@ -21,8 +21,10 @@ pub trait PseudoInverse {
 
 impl PseudoInverse for Array2<f64> {
     fn pinv(&self) -> Self {
+        // Step 0: Scale the matrix to improve numerical stability.
+        let a = *self.max().unwrap_or(&1.);
         // Step 1: Compute the Single Value Decomposition (SVD).
-        let (u, s, vt) = self.svd(true, true).unwrap_or_else(|e| {
+        let (u, s, vt) = (self / a).svd_into(true, true).unwrap_or_else(|e| {
             panic!(
                 "Failed to compute the SVD \n\
                 \t for the matrix: \n\
@@ -34,10 +36,10 @@ impl PseudoInverse for Array2<f64> {
         let u = u.expect("Failed to get U from the SVD.");
         let vt = vt.expect("Failed to get VT from the SVD.");
         // Step 2: Compute the pseudo-inverse of the singular values.
-        let smax = s.max().unwrap_or(&0.);
-        let rtol = f64::max(EPSILON, s.len() as f64 * smax * EPSILON);
-        let sinv = Array2::from_diag(&s.mapv(|x| if x > rtol { 1. / x } else { 0. }));
+        let s_max = s.max().unwrap_or(&0.);
+        let r_tol = f64::max(EPSILON, s.len() as f64 * s_max * EPSILON);
+        let s_inv = Array2::from_diag(&s.mapv(|x| if x > r_tol { 1. / x } else { 0. }));
         // Step 3: Compute the pseudo-inverse of S_zz.
-        vt.t().dot(&sinv).dot(&u.t())
+        vt.t().dot(&s_inv).dot(&u.t()) / a
     }
 }
